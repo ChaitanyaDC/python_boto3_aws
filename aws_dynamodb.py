c49b16fcb2f3604bc35ba0b7ab7956cae5688d34
Chaitanya Dayanand Chaudhari <chaitanyadchaudhari@gmail.com>
2024-02-01 13:25:58 -0800

    GET, POST,PUT, Delete on AWS DynamoDB

--- aws_dynamodb.py
+++ aws_dynamodb.py
@@ -3,10 +3,22 @@ import boto3
 db = boto3.resource('dynamodb')
 table = db.Table("employees")
 
-table.put_item(
-    Item={
-        'emp_id':"2",
-        'name':"Shraddha Khadepatil",
-        'age':"28"
+## add data to the table
+# table.put_item(
+#     Item={
+#         'emp_id':"2",
+#         'name':"Shraddha Khadepatil",
+#         'age':"28"
+#     }
+# )
+
+# get the data from the table
+response = table.get_item(
+    Key={
+        'emp_id':"2"
     }
 )
+
+print(response["Item"]["name"])
+
+
