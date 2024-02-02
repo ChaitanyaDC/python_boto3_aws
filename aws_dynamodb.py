78f7c288472dfc3b6b76f85f15705dfe2be3bcae
Chaitanya Dayanand Chaudhari <chaitanyadchaudhari@gmail.com>
2024-02-02 01:00:13 -0800

    Access AWS S3 with boto3

--- aws_dynamodb.py
+++ aws_dynamodb.py
@@ -3,19 +3,19 @@ import boto3
 db = boto3.resource('dynamodb')
 table = db.Table("employees")
 
-## add data to the table
+# add data to the table
 # table.put_item(
 #     Item={
-#         'emp_id':"2",
-#         'name':"Shraddha Khadepatil",
-#         'age':"28"
+#         'emp_id':"4",
+#         'name':"Sarvesh Kulkarni",
+#         'age':"29"
 #     }
 # )
 
 # get the data from the table
 response = table.get_item(
     Key={
-        'emp_id':"2"
+        'emp_id':"4"
     }
 )
 
